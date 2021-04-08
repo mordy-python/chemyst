9354a912a0c5c380f2b1be42791d366e4bfb366c
mordy <imky171@gmail.com>
2021-04-08 14:00:43 -0400

    Added moles to mass functions

---
+++ chemyst/moles_mass.py
@@ -0,0 +1,13 @@
+def moles_to_mass(num_moles, molar_mass):
+    grams = num_moles * molar_mass
+    return grams
+
+def mass_to_moles(g_of_element, molar_mass):
+    moles = g_of_element / molar_mass
+    return moles
+def atoms_to_grams(num_atoms, molar_mass):
+    grams = (num_atoms / (6.02*10**23)) * molar_mass
+    return grams
+def grams_to_atoms(g_of_element, molar_mass):
+    atoms = (g_of_element / molar_mass) * (6.02*10**23)
+    return atoms
